6390f56fbfae36248a9509801a3ad9c63c6c2e65
Pankaj Rana <pankaj.csdeveloper@gmail.com>
2026-08-15 16:21:22 +0530

    Analytics component has been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Home from "@/components/Home";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Office Chair Services",
 };
 
 export default async function HomePage() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 }
